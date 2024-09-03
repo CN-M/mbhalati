9cd8446b639a7eabfc82b2ba21ee22cb6fcb0264
C.N. <cn.mbhalati@gmail.com>
2024-09-03 13:05:44 +0200

    Make initial commit, add style configurations to tailwind.config.ts, and remove default NextJS boilerplate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,48 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: "#4570A1",
+      secondary: "#D4E0ED",
+      tertiary: "#5B9EEF",
+      white: "#F5F8FA",
+      "black-100": "#0E1620",
+      "black-75": "rgba(14, 22, 32, 0.75)",
+      "black-50": "rgba(14, 22, 32, 0.50)",
+      "black-25": "rgba(14, 22, 32, 0.25)",
+      "black-10": "rgba(14, 22, 32, 0.10)",
+      "black-5": "rgba(14, 22, 32, 0.5)",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        "heading-1": "3.8095rem",
+        "heading-2": "3.0476rem",
+        "heading-3": "2.4286rem",
+        "heading-4": "1.9524rem",
+        "heading-5": "1.5714rem",
+        "heading-6": "1.2381rem",
+        paragraph: "1rem",
+        "paragraph-sm": "0.8095rem",
+        "paragraph-xs": "0.6667rem",
+      },
+      lineHeight: {
+        heading: "1.2",
+        paragraph: "1.6",
+      },
+      fontFamily: {
+        heading: ["Montserrat", "sans-serif"],
+        body: ["Montserrat", "sans-serif"],
+      },
+      fontWeight: {
+        extraLight: "100",
+        thin: "200",
+        light: "300",
+        regular: "400",
+        medium: "500",
+        semiBold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
       },
     },
   },
