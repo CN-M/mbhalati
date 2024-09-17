d0d6594bec2508d592f18cd697dbb4a11d10aa0f
C.N. <cn.mbhalati@gmail.com>
2024-09-17 13:43:56 +0200

    Add metadata stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -9,10 +8,42 @@ const montserrat = Montserrat({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-export const metadata: Metadata = {
-  title: "C.N. Mbhalati | Part-Time Astronaut",
-  description: "The Profile of C.N. Mbhalati",
-};
+async function sharedMetaData() {
+  return {
+    // enable this for resolving opengraph image
+    metadataBase: new URL("https://cn.mbhalati.com/"),
+    title: {
+      default: "C.N. Mbhalati | Part-Time Astronaut",
+    },
+    description:
+      "Hey my name is C.N. I'm a Software Engineer. UI/UX Designer. Founder. And a Man Loved by Jesus.",
+    keywords: ["Warren Buffett", "Tech", "Startup", "Christian"],
+    authors: [{ name: "C.N. Mbhalati" }],
+    canonical: "https://cn.mbhalati.com/",
+    openGraph: {
+      images: [
+        {
+          url: "/opengraph.bmp",
+          // urlForImage(settings?.openGraphImage)?.src ||
+          width: 800,
+          height: 600,
+        },
+      ],
+    },
+    twitter: {
+      title: "C.N. Mbhalati | Part-Time Astronaut",
+      card: "summary_large_image",
+    },
+    robots: {
+      index: true,
+      follow: true,
+    },
+  };
+}
+
+export async function generateMetadata() {
+  return await sharedMetaData();
+}
 
 export default function RootLayout({
   children,
