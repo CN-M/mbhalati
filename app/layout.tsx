0e6cc384b57c3d8488b2b490ccf04e8742e8a229
C.N. <cn.mbhalati@gmail.com>
2024-12-18 10:06:51 +0200

    Seo stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,38 +9,66 @@ const montserrat = Montserrat({
 });
 
 async function sharedMetaData() {
+  const mainUrl = "https://cn.mbhalati.com/";
+
   return {
-    metadataBase: new URL("https://cn.mbhalati.com/"),
+    metadataBase: new URL(mainUrl),
+    alternates: {
+      canonical: "/",
+    },
     title: {
       default: "C.N. Mbhalati | Part-Time Astronaut",
+      template: "%s | C.N. Mbhalati",
     },
     description:
       "Hey. My name is C.N. I'm a Software Engineer. UI/UX Designer. Founder. Writer. And a Man Loved by Jesus.",
     keywords: [
-      "Warren Buffett",
+      "C.N. Mbhalati",
       "Tech",
       "Startup",
       "Christian",
+      "Software Engineer",
+      "UI/UX Designer",
+      "Founder",
+      "Writer",
       "Short Stories",
+      "Jesus",
+      "Faith",
     ],
     authors: [{ name: "C.N. Mbhalati" }],
-    canonical: "https://cn.mbhalati.com/",
+    canonical: mainUrl,
     openGraph: {
+      type: "website",
+      siteName: "C.N. Mbhalati",
+      locale: "en",
+      url: mainUrl,
+      title: "C.N. Mbhalati | Part-Time Astronaut",
+      description:
+        "Hey. My name is C.N. I'm a Software Engineer. UI/UX Designer. Founder. Writer. And a Man Loved by Jesus.",
       images: [
         {
-          url: "/img/opengraph.jpg",
-          width: 800,
-          height: 600,
+          url: `${mainUrl}/img/opengraph.jpg`,
+          width: 1200,
+          height: 630,
+          alt: "C.N. Mbhalati - Part-Time Astronaut",
         },
       ],
     },
     twitter: {
-      title: "C.N. Mbhalati | Part-Time Astronaut",
       card: "summary_large_image",
+      site: "@CNMbhalati",
+      title: "C.N. Mbhalati | Part-Time Astronaut",
+      description:
+        "Hey. My name is C.N. I'm a Software Engineer. UI/UX Designer. Founder. Writer. And a Man Loved by Jesus.",
+      images: [`${mainUrl}/img/twitter-card.jpg`],
     },
     robots: {
       index: true,
       follow: true,
+    },
+    other: {
+      viewport: "width=device-width,initial-scale=1",
+      charset: "UTF-8",
     },
   };
 }
