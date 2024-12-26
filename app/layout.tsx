2104d06b846a13a9c5b95191df562d9809822e3c
C.N. <cn.mbhalati@gmail.com>
2024-12-26 19:45:01 +0200

    small build bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-import { Montserrat, Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
-
-export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
